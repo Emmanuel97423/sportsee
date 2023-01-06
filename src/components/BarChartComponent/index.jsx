7b06913cfb6b4c31977b92958f5180e98fb77d0c
Emmanuel Narcisse <epokweb@hotmail.fr>
2023-01-06 08:29:34 +0400

    SPOR-18 modeling BarChart for activity list

--- src/components/BarChartComponent/index.jsx
+++ src/components/BarChartComponent/index.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable consistent-return */
 /* eslint-disable react/no-array-index-key */
 /* eslint-disable array-callback-return */
 import React, { Fragment } from 'react';
@@ -9,81 +10,110 @@ import {
   CartesianGrid,
   Tooltip,
   Legend,
-  LabelList,
+  ResponsiveContainer,
 } from 'recharts';
-import Activity from '../../models/activity/Activity';
 
-const renderCustomizedLabel = (props: any) => {
-  const { x, y, width, value } = props;
-  const radius = 10;
+function legendRenderColor(value) {
+  let isCalories = false;
+  if (value === 'calories') isCalories = true;
 
   return (
-    <g>
-      <circle cx={x + width / 2} cy={y - radius} r={radius} fill="#8884d8" />
-      <text
-        x={x + width / 2}
-        y={y - radius}
-        fill="#fff"
-        textAnchor="middle"
-        dominantBaseline="middle"
-      >
-        {value.split(' ')[1]}
-      </text>
-    </g>
+    <div>
+      {isCalories ? (
+        <span
+          style={{
+            color: ' #74798C',
+            fontFamily: 'Roboto',
+            fontSize: '14px',
+            fontWeight: '500',
+            lineHeight: '24px',
+            letterSpacing: '0px',
+            textAlign: 'left',
+          }}
+        >
+          Calories brûlées (kCal)
+        </span>
+      ) : (
+        <span
+          style={{
+            color: ' #74798C',
+            fontFamily: 'Roboto',
+            fontSize: '14px',
+            fontWeight: '500',
+            lineHeight: '24px',
+            letterSpacing: '0px',
+            textAlign: 'left',
+          }}
+        >
+          Poids (kg)
+        </span>
+      )}
+    </div>
   );
-};
+}
 
-export default function BarChart({ userActivity }) {
-  console.log('userActivity:', userActivity);
+function renderCustomizedLegend(props) {
+  const { payload } = props;
+
   return (
-    <BarChart
-      width={500}
-      height={300}
-      data={data}
-      margin={{
-        top: 5,
-        right: 30,
-        left: 20,
-        bottom: 5,
-      }}
-    >
-      <CartesianGrid strokeDasharray="3 3" />
-      <XAxis dataKey="name" />
-      <YAxis />
-      <Tooltip />
-      <Legend />
-      <Bar dataKey="pv" fill="#8884d8" minPointSize={5}>
-        <LabelList dataKey="name" content={renderCustomizedLabel} />
-      </Bar>
-      <Bar dataKey="uv" fill="#82ca9d" minPointSize={10} />
-    </BarChart>
+    <ul style={{ display: 'flex', justifyContent: 'end', gap: '30px' }}>
+      {payload.map((entry, index) => (
+        <li
+          key={`item-${index}`}
+          style={{ color: entry.color, fontSize: '14px' }}
+        >
+          {legendRenderColor(entry.value)}
+        </li>
+      ))}
+    </ul>
   );
-  // return (
-  //   <>
-  //     BarChart
-  //     {userActivity.map((element, index) => {
-  //       const { sessions } = element;
+}
+export default function BarChartComponent({ userActivity }) {
+  return (
+    <>
+      {userActivity.map((element, index) => {
+        return (
+          <Fragment key={index}>
+            <ResponsiveContainer width="100%" height={320}>
+              <BarChart
+                width={700}
+                height={400}
+                data={element.sessions}
+                margin={{
+                  top: 5,
+                  right: 30,
+                  left: 20,
+                  bottom: 5,
+                }}
+              >
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey={index} />
+                <YAxis />
+                <Tooltip />
+                <Legend
+                  content={renderCustomizedLegend}
+                  verticalAlign="top"
+                  align="right"
+                />
 
-  //       const activity = new Activity(sessions);
-  //       return activity.sessions.map((session) => {
-  //         // eslint-disable-next-line react/jsx-no-useless-fragment
-  //         return (
-  //           <Fragment key={index}>
-  //             <ul>
-  //               <li key={index}>
-  //                 <h2>day: {session.day}</h2>
-  //                 <h2>calories: {session.calories}</h2>
-  //                 <h2>kilogram: {session.kilogram}</h2>
-  //               </li>
-  //             </ul>
-  //           </Fragment>
-  //         );
-  //       });
-  //     })}
-  //   </>
-  //   // <div>{userSessions}</div>
-  //   // <LineChart width={400} height={400} data={data}>
-  //   //   <Line type="monotone" dataKey="uv" stroke="#8884d8" />
-  //   // </LineChart>
-  // );
+                <Bar
+                  dataKey="kilogram"
+                  fill="#282D30"
+                  minPointSize={5}
+                  barSize={7}
+                />
+
+                <Bar
+                  dataKey="calories"
+                  fill="#E60000"
+                  barSize={7}
+                  minPointSize={10}
+                />
+              </BarChart>
+            </ResponsiveContainer>
+          </Fragment>
+        );
+      })}
+    </>
+  );
 }
