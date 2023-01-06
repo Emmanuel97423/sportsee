3f1dbe1595cf72ba181a2ae191674e3cb955c1dd
Emmanuel Narcisse <epokweb@hotmail.fr>
2023-01-06 17:20:26 +0400

    SPOR-3 screen size OK but position issue with leftSide bar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import DefaultLayout from './layout/LayoutDefault';
 import Home from './pages/Home';
 import Dashboard from './pages/Dashboard';
 import './App.css';
-// import BarChartComponent from './components/BarChartComponent';
-// import data from './__mock__/data.json';
-
-// const userActivity = data.usersActivity;
 
 function App() {
   return (
