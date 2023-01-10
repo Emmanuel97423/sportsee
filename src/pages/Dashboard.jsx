45e0039c7c8a6433c6d81326e3a8ae5aece89ef1
Emmanuel Narcisse <epokweb@hotmail.fr>
2023-01-10 17:27:00 +0400

    Fix Barchart data modeling

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,74 +1,40 @@
 /* eslint-disable no-unused-vars */
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-// import BarChartComponent from '../components/BarChartComponent';
+import BarChartComponent from '../components/BarChartComponent';
 import RadarChartComponent from '../components/RadarChartComponent';
-// import RadarChartByChat from '../components/RadarChartByChat';
 import mockDataJson from '../__mock__/data.json';
-
-// import Performance from '../models/performances/Performance';
 
 function filterUsers(userDataFiltered, id) {
   return userDataFiltered.filter((user) => user.id === id);
 }
 
-function filterUserPerformances(userDataPerformances, id) {
-  return userDataPerformances.filter(
-    (performance) => performance.userId === id,
-  );
-}
-
 export default function Dashboard() {
   const { id } = useParams();
-  const [data, setData] = useState(mockDataJson);
-  // const userActivity = data.usersActivity;
-  const userPerformances = data.performances;
-  const userData = data.users;
+  const parseIntId = parseInt(id, 10);
 
+  const [data, setData] = useState(mockDataJson);
+  const [activityState, setActivityState] = useState(
+    mockDataJson.usersActivity,
+  );
   const [performancesState, setPerformancesState] = useState(
     mockDataJson.performances,
   );
   const [firstNameState, setFirstNameState] = useState('FirstName');
 
-  const parseIntId = parseInt(id, 10);
-
   useEffect(() => {
+    const userData = data.users;
     const filteredUser = filterUsers(userData, parseIntId);
     const { firstName } = filteredUser[0].userInfos;
     setFirstNameState(firstName);
-    // const filteredPerformances = filterUserPerformances(
-    //   userPerformances,
-    //   parseIntId,
-    // );
-
-    // const filteredPerformancesObject = filteredPerformances[0];
-
-    // setPerformancesState([...performancesState, filteredPerformancesObject]);
-
-    // const performance = new Performance(filteredPerformancesObject);
-
-    // setPerformancesState({
-    //   ...performancesState,
-    //   userId: filteredPerformancesObject.userId,
-    //   kind: filteredPerformancesObject.kind,
-    //   data: filteredPerformancesObject.data,
-    // });
-    // eslint-disable-next-line array-callback-return
-    // const activitySelectedByUserId = userActivity.filter(
-    //   (activity) => activity.userId === parseIntId,
-    // );
-    // const userSelected = userData.filter((user) => user.id === parseIntId);
-    // setFirstName(userSelected[0].userInfos.firstName);
-    // const performancesSelectedByUserId = performances.filter(
-    //   (performance) => performance.userId === parseIntId,
-    // );
-    // const performancesClasse = new Performance(performancesSelectedByUserId[0]);
   }, []);
-
+  const activitySelectedByUserId = activityState.filter(
+    (activity) => activity.userId === parseIntId,
+  );
   return (
     <div className="container">
       <h1>Bonjour {firstNameState}</h1>
-      {/* <BarChartComponent userActivity={activitySelectedByUserId} /> */}
+      <BarChartComponent userActivity={activitySelectedByUserId} />
       <RadarChartComponent
         performances={performancesState}
         userId={parseIntId}
